ead7fca25eb4df8ac8ba8e04e16c3db47becfd47
Notsamu <introduction7991@gmail.com>
2026-09-18 23:12:56 +0900

    画面遷移
    一覧に戻るボタンを作成

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate
 
 urlpatterns = [
+    path('', TodoList.as_view(), name='home'),
     path('list/', TodoList.as_view(), name='list'),
     path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
     path('create/', TodoCreate.as_view(), name='create'),
